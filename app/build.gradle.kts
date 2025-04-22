03d4c5c7a93747c1ebd7c0f6d215a6949ee5a4ed
Marion Valls <marion.valls@etu.univ-cotedazur.fr>
2025-04-22 08:55:32 +0200

    feat : adding animation on first view + adding text under menu icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "edu.polytech.concertcare"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "edu.polytech.concertcare"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -25,6 +25,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -32,29 +33,24 @@ android {
 }
 
 dependencies {
-
+    // Dépendances via le catalog libs.versions.toml
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.11.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.preference:preference:1.1.0")
-    implementation("org.osmdroid:osmdroid-android:6.0.2")
-    implementation ("com.squareup.picasso:picasso:2.71828")
 
-    implementation ("com.squareup.retrofit2:converter-jackson:2.7.2")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.10.3")
-    implementation ("com.fasterxml.jackson.core:jackson-core:2.10.3")
-    implementation ("com.fasterxml.jackson.core:jackson-annotations:2.10.3")
-    implementation ("com.google.android.gms:play-services-maps:17.0.0")
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    // Dépendances directes (non cataloguées)
+    implementation("androidx.preference:preference:1.1.0")
+    implementation("org.osmdroid:osmdroid-android:6.0.2")
+    implementation("com.squareup.picasso:picasso:2.71828")
 
+    implementation("com.squareup.retrofit2:converter-jackson:2.7.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.10.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.10.3")
+    implementation("com.google.android.gms:play-services-maps:17.0.0")
 }
